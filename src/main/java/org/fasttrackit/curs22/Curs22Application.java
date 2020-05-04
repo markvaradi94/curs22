60f34f98e781964c0ae1b18dc501af23cd7c4851
Varadi <mark.varadi03@gmail.com>
2020-05-04 18:57:43 +0300

    initial commit

---
+++ src/main/java/org/fasttrackit/curs22/Curs22Application.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.curs22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Curs22Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/fasttrackit/curs22/Curs22ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.curs22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
